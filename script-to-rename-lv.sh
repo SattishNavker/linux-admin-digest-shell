6e248711d73767de71ee100cba5504851f94e402
Satish <satish.navkar@gmail.com>
2019-10-26 08:59:46 +0530

    removed "-" as it acts like arg to command

--- script-to-rename-lv.sh
+++ script-to-rename-lv.sh
@@ -4,13 +4,14 @@
 # Here you can change variables as per your requirements.
 # This script can also be made interactive but it makes no much difference.
 #
+#
 
 $USER=`cat /etc/passwd | grep xyz | awk -F: '{ print $1 }'` #or hardcode fixed username
 $GROUP=`cat /etc/group | grep xyz | awk -F: '{ print $1 }'` #or hardcode fixed groupname
-$OLD-FS=/dev/vg/old-lv1  #replace it with your VG and LV names
-$NEW-FS=/dev/vg/new-lv1  #replace it with your VG and LV names
-$OLD-MOUNT=/xyz  #replace it with your FS
-$NEW-MOUNT=/abc  #or /xyz #replace it with your FS
+$OLDFS=/dev/vg/oldlv1  #replace it with your VG and LV names
+$NEWFS=/dev/vg/newlv1  #replace it with your VG and LV names
+$OLDMOUNT=/xyz  #replace it with your FS
+$NEWMOUNT=/abc  #or /xyz #replace it with your FS
 #if you want to USE the same old-Mount then keep Mount-name same as that of the OLD-MOUNT, ex: here it is /xyz
 
 #echo " Pre-checks Step -- mntpoint / lv - vg / user / "
@@ -27,7 +28,7 @@ $NEW-MOUNT=/abc  #or /xyz #replace it with your FS
 
 echo " Step - 1 : Unmount target file-system to be renamed, be safe "
 
-umount $OLD-MOUNT
+umount $OLDMOUNT
 
 echo " Step - 2 : Take backup of /etc/fstab - recommended"
 
@@ -35,7 +36,7 @@ cp /etc/fstab /etc/fstab-bkp-`date+"%m_%d_%Y"`
 
 echo " Step - 3 : Rename old LV to a new name using 'lvrename' command"
 
-lvrename $OLD-FS $NEW-FS
+lvrename $OLDFS $NEWFS
 
 echo " Step - 4 : Delete entry of old file-system which is renamed"
 
@@ -43,7 +44,7 @@ sed -i '$OLD-FS/d' /etc/fstab
 
 echo " Step - 5 : Add new entry in fstab for new-name of file-system"
 
-echo "$NEW-FS $NEW-MOUNT  ext4  defaults 0 0" >> /etc/fstab  #FS-type and other option can be changed as per your requirements
+echo "$NEWFS $NEWMOUNT  ext4  defaults 0 0" >> /etc/fstab  #FS-type and other option can be changed as per your requirements
 
 echo " Step - 6 : Mount renamed file-system again to start using it"
 
@@ -52,8 +53,8 @@ mount -a # mount -a will mount all FS mentioned in /etc/fstab, if you dont want 
 
 echo " Step - 7 : Change ownership of mount"
 
-chown $USER:$GROUP $NEW-MOUNT # some of us might want to chown mounts, depending on your enviroments
+chown $USER:$GROUP $NEWMOUNT # some of us might want to chown mounts, depending on your enviroments
 
 echo " Step - 8 : Verification of changes"
 
-df -h $NEW-MOUNT;echo;ls -ld $NEW-MOUNT;echo;cat /etc/fstab | grep $NEW-MOUNT
+df -h $NEWMOUNT;echo;ls -ld $NEWMOUNT;echo;cat /etc/fstab | grep $NEWMOUNT
